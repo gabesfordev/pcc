4fab6e2f09e85789362c5c45031d73edd8ff9d15
gabesfordev <gabesfordev@gmail.com>
2021-10-20 17:43:46 +0400

    ch4

---
+++ ch4/ex_one_million.py
@@ -0,0 +1,9 @@
+""" One Million:
+        Make a list of the numbers from one to one million, and then
+        use a for loop to print the numbers. (If the output is taking too long, stop it by
+        pressing ctrl -C or by closing the output window.)
+"""
+
+# if it takes too long press: (ctrl -C) to stop iterations
+for num in list(range(1000001)):
+    print(num)
