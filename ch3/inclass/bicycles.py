1a3e061c5ac5666c8ab5889d4fa56ac7d75609a2
gabesfordev <gabesfordev@gmail.com>
2021-10-22 20:09:28 +0400

    add names.py

---
+++ ch3/inclass/bicycles.py
@@ -0,0 +1,4 @@
+
+# index    0        1         2          3
+bicks = ['trek', 'sport', 'special', 'off-road']
+
---
+++ ch3/inclass/names.py
@@ -0,0 +1,5 @@
+my_friends = ['alice', 'bob', 'mark']
+
+print(my_friends[0])
+print(my_friends[1])
+print(my_friends[2])
