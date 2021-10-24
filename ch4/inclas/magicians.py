ba691cd136637564aca81310cdd560a7e02b5c83
gabesfordev <gabesfordev@gmail.com>
2021-10-24 13:38:55 +0400

    add pizzas.py

---
+++ ch4/inclas/magicians.py
@@ -0,0 +1,12 @@
+magicians = ['alice', 'bob', 'carolina']
+
+# print(f'Hello {magicians[0]}')
+# print(f'Hello {magicians[1]}')
+# print(f'Hello {magicians[2]}')
+
+for magician in magicians:
+    print(f'{magician.title()} that was great trick')
+
+    print('I can\'t waight to see your next trick, ' + magician.title() + '.\n')
+
+# print('Thank you everyone that was greate magic show.')
