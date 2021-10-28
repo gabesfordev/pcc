39cb8578806711ed7de677a667c23c5ff429a736
gabesfordev <gabesfordev@gmail.com>
2021-10-28 19:40:04 +0400

    rename stages of life

---
+++ ch5/ex_alien_color1.py
@@ -0,0 +1,5 @@
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('You just earned 5 points.')
+
--- ch5/ex_alien_color3.py
+++ ch5/ex_alien_color3.py
@@ -1,8 +1,10 @@
-alien_color = 'red'
+alien_color = 'green'
 
 if alien_color == 'green':
     print('You just earned 5 points.')
 elif alien_color == 'yellow':
     print('You just earned 10 points.')
-elif alien_color == 'red':
+else:
     print('You just earned 15 points.')
+
+
