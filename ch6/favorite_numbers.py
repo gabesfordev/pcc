e38b7a7b32f832acad5c792bcb8216ec509557fa
gabesfordev <gabesfordev@gmail.com>
2021-11-04 20:37:18 +0400

    add favorite_numbers.py

---
+++ ch6/favorite_numbers.py
@@ -0,0 +1,13 @@
+fav_nums = {
+    'alice': 5,
+    'bob': 3,
+    'morgan': 8,
+    'joe': 2,
+    'greta': 13,
+}
+
+print('alice favorite number is {}'.format(fav_nums['alice']))
+print('bob favorite number is {}'.format(fav_nums['bob']))
+print('morgan favorite number is {}'.format(fav_nums['morgan']))
+print('joe favorite number is {}'.format(fav_nums['joe']))
+print('greta favorite number is {}'.format(fav_nums['greta']))
