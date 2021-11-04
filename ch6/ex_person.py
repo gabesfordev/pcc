9a1fcf107368a2292de3534e6f9bcd7e57ad51f3
gabesfordev <gabesfordev@gmail.com>
2021-11-04 20:26:24 +0400

    add ex_person.py

---
+++ ch6/ex_person.py
@@ -0,0 +1,16 @@
+person = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'age': 23,
+    'city': 'New-York',
+}
+
+first_name = person['first_name']
+last_name = person['last_name']
+age = person['age']
+city = person['city']
+
+print(first_name)
+print(last_name)
+print(age)
+print(city)
