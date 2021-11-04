1875430cfe7b10e6e42d7d47c5480bfeffbd491e
gabesfordev <gabesfordev@gmail.com>
2021-11-04 20:20:58 +0400

    add fav-lang

---
+++ ch6/favorite_languages.py
@@ -0,0 +1,8 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+print('Sarah\'s favorite language is ' + favorite_languages['sarah'].title() + '.')
